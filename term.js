984d6ec24537ad61f0fe94ae992967bc9541d500
michael dimmitt <michaelgdimmitt@gmail.com>
2019-05-07 22:57:55 -0400

    exec and spawn example

--- term.js
+++ term.js
@@ -1,11 +1,33 @@
-async function term(args) {
-  string_construction(args);
+const { spawn, exec } = require('child_process');
+
+function execute(info){
+  console.log({info})
+    const ls = spawn(info);
+    
+    ls.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    })
 }
 function string_construction(args){
   let str = '';
   for (i = 0; i < args.length; i++) {
-    str = str + args[i] + " ";
+    str = str + args[i] + "";
   }
-  console.log("\ncommand executed,", '{str}', "\n");
+  return str
 }
-term(process.argv.slice(2));
+
+console.log(
+  execute(
+  string_construction(
+    process.argv.slice(2)
+  ))
+)
+
+exec('find . -type f | wc -l; ls', (err, stdout, stderr) => {
+  if (err) {
+    console.error(`exec error: ${err}`);
+    return;
+  }
+
+  console.log(`Number of files ${stdout}`);
+});
