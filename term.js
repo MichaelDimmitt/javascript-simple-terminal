bd21e7c717c69ae3ee979cd2848ea53a8dc21677
michael dimmitt <michaelgdimmitt@gmail.com>
2019-05-08 00:54:28 -0400

    handle when no argumentas were passed into the application.

--- term.js
+++ term.js
@@ -1,33 +1,47 @@
 const { spawn, exec } = require('child_process');
 
-function execute(info){
-  console.log({info})
-    const ls = spawn(info);
-    
-    ls.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
-    })
+const execute = (info) => {
+ exec(info, (err, stdout, stderr) => {
+   if (err) {
+     console.error(`exec error: ${err}`);
+     return;
+   }
+   else if (stdout) {
+     console.log(stdout);
+   }
+   else {
+     console.log(stderr)
+   }
+ });
+ 
+ 
 }
-function string_construction(args){
+
+const string_construction = (args) => {
   let str = '';
   for (i = 0; i < args.length; i++) {
     str = str + args[i] + "";
   }
+  str=str ? str : 'echo ;' 
   return str
 }
 
-console.log(
-  execute(
-  string_construction(
-    process.argv.slice(2)
-  ))
-)
+const terminal = async () => {
+  return await execute(
+    string_construction(
+      process.argv.slice(2)
+    )
+  ) 
+}
+terminal()
 
-exec('find . -type f | wc -l; ls', (err, stdout, stderr) => {
-  if (err) {
-    console.error(`exec error: ${err}`);
-    return;
-  }
-
-  console.log(`Number of files ${stdout}`);
-});
+// exec('npx @availity/workflow init info --package angular', (err, stdout, stderr) => {
+//   if (err) {
+//     console.error(`exec error: ${err}`);
+//     return;
+//   }
+// 
+//   console.log(`Number of files ${stdout}`);
+// });
+// 
+// 
